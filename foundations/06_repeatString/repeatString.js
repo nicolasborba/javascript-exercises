795680e83a42e5994e8b084f499525cffeb33e13
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2025-12-26 05:29:40 -0300

    Adjust code, complete first step of challenge

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+let string = 'hey'
+let num = 3
+let multipliedString = ""
 
+const repeatString = function (s, x) {
+
+  if (x > 0) {
+    x--;
+    multipliedString = multipliedString + s;
+    repeatString(s, x);
+    return multipliedString;
+  } else {
+    return multipliedString;
+  }
 };
 
 // Do not edit below this line
