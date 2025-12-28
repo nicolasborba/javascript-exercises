41d6d5d8946354029baf1578bf92bcfd8868d678
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2025-12-27 22:15:56 -0300

    Make initial adjustments to progress in challenge

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,28 @@
-const reverseString = function() {
+const reversePrint = function(charactersLeft, string, reverseResult) {
 
+  if (charactersLeft > 0) {
+
+    reverseResult = reverseResult + string[charactersLeft-1];
+    charactersLeft--;
+    
+    reversePrint(charactersLeft, string, reverseResult);
+    return reverseResult;
+
+  } else {
+    return reverseResult;
+  }
+
+}
+
+const reverseString = function(string) {
+
+let stringLength = string.length;
+let charactersLeft = stringLength;
+let reverseResult = '';
+
+reversePrint(charactersLeft, string, reverseResult);
+
+ 
 };
 
 // Do not edit below this line
