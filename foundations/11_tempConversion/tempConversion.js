c7981a31457b1a14372e2c4b2bbd4b5e283d94ce
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-01-10 09:21:02 -0300

    Adjust the code to complete the challenge

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+
+  let celsius = +((temperature - 32) * (5/9)).toFixed(1);
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+
+  let farenheit = +(temperature * (9/5) + 32).toFixed(1);
+  return farenheit;
+
+
 };
 
 // Do not edit below this line
