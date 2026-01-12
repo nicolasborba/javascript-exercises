c91659a7a1ef67f0dfa149fad1915ea669d6c4bf
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-01-12 03:28:08 -0300

    Configure the code to meet challenge expectations

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,77 @@
-const add = function() {
+const add = function(a, b) {
+
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(m, n) {
 	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return m - n;
 
 };
 
-const power = function() {
+const sum = function(arraySum) {
+
+  let lengthSum = arraySum.length;
+
+  let resultSum = 0;
+
+  for (let x = lengthSum - 1; x >= 0; x--) {
+
+    resultSum += arraySum[x];
+
+  }
+
+  return resultSum;
 	
 };
 
-const factorial = function() {
+const multiply = function(arrayMultiply) {
+  
+  let lengthMultiply = arrayMultiply.length;
+
+  let resultMultiply = 1;
+
+  for (let y = lengthMultiply - 1; y >= 0; y--) {
+
+    resultMultiply *= arrayMultiply[y];
+
+  }
+
+  return resultMultiply;
+
+};
+
+const power = function(base, exponent) {
+
+  return base ** exponent;
 	
+};
+
+const factorial = function(f) {
+	
+  if (f < 0) {
+
+    return "ERROR"
+
+  } else if (f == 0) {
+
+    return 1;
+
+  } else {
+
+    let factorialResult = f;    
+
+    for (let g = factorialResult - 1; g > 0; g--) {
+
+      factorialResult *= g;
+
+    }
+
+    return factorialResult;
+
+  }
+
 };
 
 // Do not edit below this line
