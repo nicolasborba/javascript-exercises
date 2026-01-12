c427aaecade17fa95bd70f3ed0106daf6716322e
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-01-12 05:21:16 -0300

    Adjust the function to fulfill all criteria

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+  let noSpecialCharacters = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  let reversedString = "";
+
+  let stringLength = noSpecialCharacters.length;
+
+  for (x = stringLength - 1; x >= 0; x--) {
+
+    reversedString += noSpecialCharacters[x];
+
+  }
+  
+  return reversedString === noSpecialCharacters;
 
 };
 
