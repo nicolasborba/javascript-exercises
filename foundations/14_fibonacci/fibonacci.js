6aaeaa06ac5622f39d4ab3181bfe2db0b6153b68
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-01-13 01:44:39 -0300

    Adjust the function to output fibonacci numbers

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,50 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+
+  let currentNumber = 0;
+
+
+  if (x < 0) {
+
+    return "OOPS";
+
+  } else if (x == 0) {
+
+    currentNumber = 0;
+
+  } else if (x <= 2) {
+
+    currentNumber = 1;
+
+  } else {
+
+    let precedingNumberOne = 1;
+
+    let precedingNumberTwo = 1;  
+
+    currentNumber = 1;
+
+    let preIteratingCurrentNumber = currentNumber;
+
+    for (let iterations = 3; iterations <= x; iterations++) {
+
+      preIteratingCurrentNumber = currentNumber;
+
+      currentNumber += precedingNumberOne;
+
+      precedingNumberTwo = precedingNumberOne;
+
+      precedingNumberOne = preIteratingCurrentNumber;
+
+    }
+
+    return currentNumber;
+
+  }
+
+  return currentNumber;
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
