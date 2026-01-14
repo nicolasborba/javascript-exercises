3af3685e18c7961ad657ff273a08c56ba494faab
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-01-14 04:25:12 -0300

    Adjust function based on challenge requirements

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+
+  let arrayLength = array.length;
+
+  let bookArray = [];
+
+  for (let x = 0; x <= arrayLength - 1; x++) {
+
+    bookArray.push(array[x].title);
+
+  }
+
+  return bookArray;
+
+// use object.keys e array keys
+
 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
