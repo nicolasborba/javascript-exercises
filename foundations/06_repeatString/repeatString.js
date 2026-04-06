8273909072b3cffbf8de694d78e1b461dd85321f
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-04-05 23:18:55 -0300

    Adjust the function to complete exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+  
+
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let repeatedString = '';
+    
+    for (let i = 1; i <= num; i++) {
+
+      repeatedString += string;
+
+    }
+
+    return repeatedString;
+
+  }
+
 
 };
 
