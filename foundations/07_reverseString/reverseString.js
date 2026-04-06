b0e94cdbfe168d607d868debdc7c2d573c3570ec
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-04-05 23:40:56 -0300

    Adjust function to fulfill exercise 07 requirement

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+  let reversedString = '';
+  let reverseIndex = 0;
+
+  for (let i = (string.length - 1); i >= 0; i--) {
+
+    reversedString += string[i];
+    reverseIndex++;
+
+  }
+
+  return reversedString;
 
 };
 
