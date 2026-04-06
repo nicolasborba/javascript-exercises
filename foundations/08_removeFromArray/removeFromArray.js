8d05aaf267005be2bdef3878acc8d3f8463a9ffa
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-04-06 00:14:36 -0300

    Develop function to get closer to the objective

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+
+  let resultArray = array;
+
+  for (let i = 1; i < arguments.length; i++) {
+
+    //search for arguments[i] in array
+
+    let foundIndex = resultArray.findIndex(item => item == arguments[i]);
+
+    if ((foundIndex) != -1) {
+
+        //if find, splice removing that element
+
+        resultArray.splice(foundIndex, 1);
+
+    }
+    
+
+  }
+
+  return resultArray;
+
 };
 
 // Do not edit below this line
