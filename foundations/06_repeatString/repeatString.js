75de9682594e2cd4c85253f5aea5bcbb31d1e333
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-04-05 22:53:33 -0300

    Clear exercise 6 in the intention of re-doing it

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,25 +1,5 @@
-let multipliedString;
+const repeatString = function() {
 
-const loopString = function (s, x) {
-    if (x > 0) {
-    x--;
-    multipliedString = multipliedString + s;
-    loopString(s, x);
-    return multipliedString;
-  } else {
-    return multipliedString;
-  }
-  }
-
-const repeatString = function (s, x) {
-
-  if (x >= 0) {
-  multipliedString = "";
-  return loopString(s, x);
-  } else {
-    return 'ERROR';
-  }
- 
 };
 
 // Do not edit below this line
