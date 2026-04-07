6d40d6cb8c6f852b8d1bb0ef9f903c457208a829
nicolasborba <78428173+nicolasborba@users.noreply.github.com>
2026-04-06 23:44:27 -0300

    Adjust the function so it passes all tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,46 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+
+  let sum = 0;
+  let lowest;
+  let highest;
+
+  // determine if a > b or b > a, to decide the order
+
+  if (!Number.isInteger(a)
+    || !Number.isInteger(b)
+    || a <= 0
+    || b <= 0) {
+    return "ERROR";
+  }
+
+  if (a > b) {
+
+    lowest = b;
+    highest = a;
+
+  } else if (b > a) {
+
+    lowest = a;
+    highest = b;
+
+  } else {
+
+    return a;
+
+  }
+
+  // loop from the lower while it is lower or equal
+
+  for (lowest; lowest <= highest; lowest++) {
+
+    sum += lowest;
+
+  }
+
+  // ++ the lower after each iteration
+  // sum the lower to the `sum` var
+
+  return sum;
 
 };
 
